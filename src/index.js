8f4b4fe267718375a2a64194b135846d9609eaf8
Andrey <ScootalooRainbow@yandex.ru>
2021-06-21 19:31:19 +0900

    moved variables to turnOnForm scope

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import FormCreator from './modules/FormCreator';
 import data from './data.json';
 
 
-const options = {
-  data: data,
-  place: '.content',
-  customFormTag: 'custom-form',
-  changeThemeSelector: 'change_theme',
-}
-
-
-const form = new FormCreator(options.data, options.place, options.customFormTag, options.changeThemeSelector);
 
 function turnOnForm() {
+  const options = {
+    data: data,
+    place: '.content',
+    customFormTag: 'custom-form',
+    changeThemeSelector: 'change_theme',
+  };
+
+  const form = new FormCreator(
+    options.data,
+    options.place,
+    options.customFormTag,
+    options.changeThemeSelector
+  );
   form.connectedCallback();
 }
 window.addEventListener('DOMContentLoaded', function (event) {
